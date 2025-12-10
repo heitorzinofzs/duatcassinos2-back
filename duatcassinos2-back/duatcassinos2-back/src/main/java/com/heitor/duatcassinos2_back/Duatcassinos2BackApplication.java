ffb227ce1b4f83949acd3ab4e27dd97d16037319
heitorzinofzs <walterapj45@gmail.com>
2025-12-10 00:44:43 -0300

    configurando o spring

---
+++ duatcassinos2-back/duatcassinos2-back/src/main/java/com/heitor/duatcassinos2_back/Duatcassinos2BackApplication.java
@@ -0,0 +1,13 @@
+package com.heitor.duatcassinos2_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Duatcassinos2BackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Duatcassinos2BackApplication.class, args);
+	}
+
+}
---
+++ duatcassinos2-back/duatcassinos2-back/src/test/java/com/heitor/duatcassinos2_back/Duatcassinos2BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heitor.duatcassinos2_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Duatcassinos2BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
